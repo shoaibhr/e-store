f8974e0369e822b4d1b29d024877fffd72ea3f05
shoaib <shoaibcie@gmail.com>
2024-08-14 16:36:23 +0300

    create custom user model(email and mobile number added)

--- estore/settings.py
+++ estore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
